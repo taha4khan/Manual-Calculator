26538ef6e76186d0ed0dd53db4178c7de368a154
Taha Khan <102149347+taha4khan@users.noreply.github.com>
2022-03-23 18:25:21 +0500

    Add files via upload

---
+++ calculator manual.py
@@ -0,0 +1,14 @@
+a=int(input("first value:"))
+b=int(input("second value:"))
+def practice(x,y,u,o,v,t,r,e):
+    print("first value:",x)
+    print("second value:",y)
+    result=x+y
+    result1=u-o
+    result2=v*t
+    result3=r/e
+    print("the final sum is:",result)
+    print("the final substract is:",result1)
+    print("the final multiplication is:",result2)
+    print("the final division is:",result3)
+practice(a,b,1,2,3,4,5,6)
